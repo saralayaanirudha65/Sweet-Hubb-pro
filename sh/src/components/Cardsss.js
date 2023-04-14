f57d533e7bf933e3a6f727e2d9f1041b59312e1a
ani <ani>
2023-04-14 16:30:49 +0530

    sec com

--- sh/src/components/Cardsss.js
+++ sh/src/components/Cardsss.js
@@ -16,7 +16,7 @@ function Cards() {
             <h2 className="text-center text-white">Sweet Shops </h2>
             
             <CardGroup >
-                
+        
                 <Card >
                 <Card.Body className=" text-white">
                  
@@ -99,7 +99,7 @@ function Cards() {
                         </Card.Text>
 
                         <Card.Text>
-                        <BsFillTelephoneFill/><b>PHONE NO </b> 9448691137 
+                        <BsFillTelephoneFill/><b>PHONE NO </b> 07947123429
                         </Card.Text>
                     </Card.Body>
                     <Card.Footer>
@@ -117,6 +117,208 @@ function Cards() {
                     
                 </Card>
             </CardGroup>
+
+            
+            <CardGroup >
+                
+                <Card >
+                <Card.Body className=" text-white">
+                 
+                <Card.Title  ><BsShop/> Shop Name</Card.Title>
+                        <Card.Text>
+                           <b> OM UTSAV SWEETS</b>
+                           <img
+          className="d-block w-50"
+          src="https://lh3.googleusercontent.com/p/AF1QipMTQh-YdQdl1WnNT2g3mvZh3jeKLN-jZfwDPLwZ=s680-w680-h510"
+          
+        />
+
+
+                            
+                        </Card.Text>
+                        <Card.Text>
+                            <b>ADDRESS</b> 8QW6+JCF, Udupi-Manipal Hwy, Kunjibettu <br></br>Udupi, Karnataka 576102 <br></br>
+                           
+                        </Card.Text>
+                        <Card.Text>
+                        <BsFillTelephoneFill/><b>PHONE NO </b> 7947331918
+                        </Card.Text>
+                        
+                    </Card.Body>
+                    <Card.Footer>
+                    <Button variant="outline-warning">
+                           <a href="https://goo.gl/maps/zMKoREMxaCULxhfZA" ><MdLocationOn/> Address</a>
+                        </Button>
+                    </Card.Footer>
+                    
+                </Card>
+                <Card>
+                
+
+                    <Card.Body className=" text-white">
+                        <Card.Title> <BsShop/> Shop Name</Card.Title>
+                        <Card.Text>
+                           <b> BIG MISHRA PEDHA</b>
+                           <img
+          className="d-block w-50"
+          src="https://github.com/saralayaanirudha65/ne/blob/master/MISHRA.jpg?raw=true"
+          
+        />
+                            
+                        </Card.Text>
+                        <Card.Text>
+                            <b>ADDRESS</b>  London Plaza, Km Marg, udupi - 576101 (Near London Opticals&London Times)<br></br>
+                           
+                        </Card.Text>
+
+                        <Card.Text>
+                     <BsFillTelephoneFill/>  <b>PHONE NO </b> 7947225669
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                    <Button variant="outline-warning">
+                           <a href="https://goo.gl/maps/iLyN95iZyzyhmcpp6" ><MdLocationOn/> Address</a>
+                        </Button>
+                    </Card.Footer>
+                </Card>
+                
+
+                    
+                <Card >
+                <Card.Body className=" text-white">
+                 
+                <Card.Title  ><BsShop/> Shop Name</Card.Title>
+                        <Card.Text>
+                           <b> UDUPI SWEETS </b>
+                           <img
+          className="d-block w-50"
+          src="https://lh3.googleusercontent.com/p/AF1QipOjpVVO01wbsIz4YhUHMZA3ISG-2kVR99lHhEEF=s680-w680-h510"
+          
+        />
+                            
+                        </Card.Text>
+                        <Card.Text>
+                            <b>ADDRESS</b> No.9-4-44e, Sri Krishna Sowrabha, Km Marg, Udupi - 576101 (Opposite Mother Of Sorrowas Church)<br></br>
+                           
+                        </Card.Text>
+
+                        <Card.Text>
+                        <BsFillTelephoneFill/><b>PHONE NO </b> 7947422893
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                    <Button variant="outline-warning">
+                           <a href="https://goo.gl/maps/vk5tqjJnh6fi51MW8" ><MdLocationOn/> Address</a>
+                        </Button>
+                    </Card.Footer>
+                    </Card>
+            </CardGroup>
+
+
+            <CardGroup >
+        
+                <Card >
+                <Card.Body className=" text-white">
+                 
+                <Card.Title  ><BsShop/> Shop Name</Card.Title>
+                        <Card.Text>
+                           <b>SRI SAI PARIVAR</b>
+                           <img
+          className="d-block w-50"
+          src="https://github.com/saralayaanirudha65/ne/blob/master/SS.jpg?raw=true"
+          
+        />
+
+
+                            
+                        </Card.Text>
+                        <Card.Text>
+                            <b>ADDRESS</b> 8PVW+2RJ, Thenkpete, Maruthi Veethika, <br></br> Udupi, Karnataka 576101 <br></br>
+                           
+                        </Card.Text>
+
+                     
+                    </Card.Body>
+                    <Card.Footer>
+                    <Button variant="outline-warning">
+                           <a href="https://goo.gl/maps/vk5tqjJnh6fi51MW8" ><MdLocationOn/> Address</a>
+                        </Button>
+                    </Card.Footer>
+                    
+                </Card>
+                <Card>
+                
+
+                    <Card.Body className=" text-white">
+                        <Card.Title> <BsShop/> Shop Name</Card.Title>
+                        <Card.Text>
+                           <b> MADHURAM MISTHAN BHANDHAR</b>
+                           <img
+          className="d-block w-50"
+          src="https://lh3.googleusercontent.com/p/AF1QipPKTSl_9vpdzl8YHZaHFNaIjeaj_mgoFS9-YyIG=s680-w680-h510"
+          
+        />
+                            
+                        </Card.Text>
+                        <Card.Text>
+                            <b>ADDRESS</b> Ground floor , Hotel White Lotus, Maruthi Veethika Rd, Udupi, Karnataka 576101<br></br>
+                           
+                        </Card.Text>
+
+                        <Card.Text>
+                     <BsFillTelephoneFill/>  <b>PHONE NO </b> 090353 46154
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                    <Button variant="outline-warning">
+                           <a href="https://goo.gl/maps/YDZNaX6oDth4VnZQ8" ><MdLocationOn/> Address</a>
+                        </Button>
+                    </Card.Footer>
+                </Card>
+                
+
+                    
+                <Card >
+                <Card.Body className=" text-white">
+                 
+                <Card.Title  ><BsShop/> Shop Name</Card.Title>
+                        <Card.Text>
+                           <b> PARIVAR SWEETS AND BAKERY</b>
+                           <img
+          className="d-block w-50"
+          src="http://parivarudupi.com/images/300.jpg"
+          
+        />
+                            
+                        </Card.Text>
+                        <Card.Text>
+                            <b>ADDRESS</b> Sanskrit College Road ,Udupi,
+      Karnataka ( South ) - 576101<br></br>
+                           
+                        </Card.Text>
+
+                        <Card.Text>
+                        <BsFillTelephoneFill/><b>PHONE NO </b>  0820-2525101
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                    <Button variant="outline-warning">
+                           <a href="https://goo.gl/maps/uMBPCrTaLBpyVqka6" ><MdLocationOn/> Address</a>
+                        </Button>
+                    </Card.Footer>
+
+
+                    
+
+                    
+
+                    
+                    
+                </Card>
+            </CardGroup>
+
+
+
         </>
     );
 }
